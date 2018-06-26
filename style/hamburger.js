c486a06758c4a21da7f5b03ad7c2e139c363726c
jean-rosset-esdes <37215884+jean-rosset-esdes@users.noreply.github.com>
2018-06-26 10:45:27 +0200

    Add files via upload

---
+++ style/hamburger.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function hamburger_click()
+{
+  var hamburgerElement = document.getElementById("hamburger");
+  var menuElement = document.getElementById("menu");
+ 
+  hamburgerElement.style.display = "none";
+  menuElement.style.display = "block";
+}
